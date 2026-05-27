fc11a42320fefc8e9fb02e127e0b710e4bc6564f
missmioh <186388075+missmioh@users.noreply.github.com>
2026-05-27 11:08:27 +0200

    Store color scheme for easy alteration

--- lib/view_models/app_view_model.dart
+++ lib/view_models/app_view_model.dart
@@ -5,6 +5,11 @@ import 'package:flutter_todo/models/user_model.dart';
 class AppViewModel extends ChangeNotifier {
   List<Task> tasks = <Task>[];
   User user = User("Milly");
+
+  Color colorLvl1 = Colors.amber.shade100;
+  Color colorLvl2 = Colors.amber.shade200;
+  Color colorLvl3 = Colors.amber.shade300;
+  Color colorLvl4 = Colors.amber.shade600;
 }
 
 // tasks are gonna be stored here
