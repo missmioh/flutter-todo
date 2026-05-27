a35c1d7d2af72c59ed539b3ef45de381bce256f7
missmioh <186388075+missmioh@users.noreply.github.com>
2026-05-27 11:01:26 +0200

    Connect main to AppViewModel

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_todo/view_models/app_view_model.dart';
 import 'package:flutter_todo/views/task_page.dart';
+import 'package:provider/provider.dart';
 
 void main() {
 
-  runApp( AddictiveTasks() );
+  runApp(ChangeNotifierProvider(create: (context) => AppViewModel(), child: const AddictiveTasks()));
 }
 
 class AddictiveTasks extends StatelessWidget {
